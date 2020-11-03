230821834fadba0cb8b69156e066e535be4f271e
Kanefav <gabrielfav12@gmail.com>
2020-11-03 13:33:05 -0300

    Third Version of My First Terminal RPG Game

--- PyGames/jogo_rpg_skypia.py
+++ PyGames/jogo_rpg_skypia.py
@@ -1,30 +1,40 @@
 from time import sleep
 
+TempInimigoHeal = 0
+TempProtaHeal = 0
+
 
 #Poderes Base Servant
 PowerServant = {
-'PowerServantHeal': 50,
-'PowerServantAtq': 20}
+'PowerProtaHeal': 50,
+'PowerProtaAtq': 20}
 
 #Poderes Base Logia
 PowerLogia = {
-'PowerLogiaHeal': 80,
-'PowerLogiaAtq': 10}
+'PowerProtaHeal': 80,
+'PowerProtaAtq': 10}
 
 #Poderes Base Arcanne
 PowerArcanne = {
-'PowerArcanneHeal': 60,
-'PowerArcanneAtq': 15}
+'PowerProtaHeal': 60,
+'PowerProtaAtq': 15}
 
 #Inimigo 1
 PowerFirstInimigo = {
+'InimigoName': 'FairGil',
 'PowerInimigoHeal': 40,
 'PowerInimigoAtq': 5}
 
+#Inimigo 2
+PowerSecondInimigo = {
+'InimigoName': 'Illya-san',
+'PowerInimigoHeal': 80,
+'PowerInimigoAtq': 15}
+
 print('Bem Vindo ao Jogo de RPG Skypia')
 PrimeiroInimigo = {'Nome':'FairGil', 'Classe':'Inimigo1'}
-Prota = {'Nome':' ','Classe':''}
-Prota['Nome'] = input(str('Digite Seu Nome:'))
+Protagonista = {'Nome':' ','Classe':''}
+Protagonista['Nome'] = input(str('Digite Seu Nome:'))
 sleep(1)
 print('Escolha uma classe:\n'
       'Digite 1 para>> Servant: Você será como um Summoner, Invocara um Servo para que lute por você\n'
@@ -32,43 +42,66 @@ print('Escolha uma classe:\n'
       'Digite 3 Para>> Arcanne: Você Será Como um Encantador, Poderá se Tornar um Servo')
 ProtaTempClass = input(str('Escolha uma Classe: '))
 if ProtaTempClass == '1':
-    Prota['Classe'] = 'Servant'
+    Protagonista['Classe'] = 'Servant'
 if ProtaTempClass == '2':
-    Prota['Classe'] = 'Logia'
+    Protagonista['Classe'] = 'Logia'
 if ProtaTempClass == '3':
-    Prota['Classe'] = 'Arcanne'
+    Protagonista['Classe'] = 'Arcanne'
 
 
-def BattleServantOne(Prota, Inimigo):
-    for c in range(0, 1):
-         Inimigo['PowerInimigoHeal'] -= Prota['PowerServantAtq']
-    print(f'Você Deu {Prota["PowerServantAtq"]} De ATQ no FairGil(Inimigo1) que Agora está com {Inimigo["PowerInimigoHeal"]} de Vida')
+def Battle(Prota, Inimigo,):
+            RoundCont = 0
+            TempInimigoHeal = Inimigo['PowerInimigoHeal']
+            TempProtaHeal = Prota['PowerProtaHeal']
+            while TempInimigoHeal or TempProtaHeal == 0:
+                if RoundCont ==  0:
+                    input('Press Enter To Start Battle')
 
+                TempInimigoHeal -= Prota['PowerProtaAtq']
+                #Dano Protagonista
+                print(f'{Protagonista["Nome"]} Deu {Prota["PowerProtaAtq"]}'
+                      f' de Dano no {Inimigo["InimigoName"]} que Agora Está com {TempInimigoHeal} de Vida')
+                if TempInimigoHeal <= 0:
+                    return print(f'Parabéns Você ({Protagonista["Nome"]}) Ganhou, A batalha durou {RoundCont} Rodadas ')
+                input('Press Enter to Continue')
+                sleep(1)
 
-def BattleLogiaOne(Prota, Inimigo):
-    for c in range(0,1):
-        Inimigo['PowerInimigoHeal'] -= Prota['PowerLogiaAtq']
-    print(f'Você deu {Prota["PowerLogiaAtq"]} De ATQ no FairGil que Agora está com {Inimigo["PowerInimigoHeal"]} de Vida')
+                TempProtaHeal -= Inimigo['PowerInimigoAtq']
+                #Dano Inimigo
+                print(f'{Inimigo["InimigoName"]} Deu {Inimigo["PowerInimigoAtq"]}'
+                      f' de Dano no {Protagonista["Nome"]} que Agora Está com {TempProtaHeal} de Vida')
+                RoundCont += 1
+                if TempProtaHeal <= 0:
+                    return print(f'Você perdeu, A Batalha durou {RoundCont} Rodadas')
+                input('Press Enter to Continue')
+                sleep(1)
+            if TempInimigoHeal == 0:
+                print(f'Parabéns Você ({Protagonista["Nome"]}) Ganhou, A batalha durou {RoundCont} ')
+            else:
+                print('Você perdeu')
 
-
-def BattleArcanneOne(Prota, Inimigo):
-    for c in range(0,1):
-        Inimigo['PowerInimigoHeal'] -= Prota['PowerArcanneAtq']
-    print(f'Você deu {Prota["PowerArcanneAtq"]} De ATQ no FairGil que Agora está com {Inimigo["PowerInimigoHeal"]} de Vida')
 
 
 Action = input(str('Agora que Você Criou seu Personagem Pode Fazer Algumas Açoes:\n'
         'Digite Pro Para>> Visualizar o Próprio Perfil\n'
-        'Digite Battle Para>> Lutar com algum Inimigo')).capitalize()
+        'Digite Battle Para>> Lutar com algum Inimigo ')).capitalize()
 if Action == 'Pro':
-    print(Prota)
+    print(Protagonista)
 if Action == 'Battle':
-    TempInimigo = input(str('Escolha algum Inimigo:'
-                            'Digite 1 Para>> Inimigo Mais Fraco'))
+    TempInimigo = input(str('Escolha algum Inimigo:\n'
+                            'Digite 1 Para>> Inimigo Fraco\n'
+                            'Digite 2 Para>> Inimigo Medio'))
     if TempInimigo == '1':
-        if Prota['Classe'] == 'Servant':
-            BattleServantOne(PowerServant, PowerFirstInimigo)
-        if Prota['Classe'] == 'Logia':
-            BattleLogiaOne(PowerLogia, PowerFirstInimigo)
-        if Prota['Classe'] == 'Arcanne':
-            BattleArcanneOne(PowerArcanne, PowerFirstInimigo)
+        if Protagonista['Classe'] == 'Servant':
+            Battle(PowerServant, PowerFirstInimigo)
+        if Protagonista['Classe'] == 'Logia':
+            Battle(PowerLogia, PowerFirstInimigo)
+        if Protagonista['Classe'] == 'Arcanne':
+            Battle(PowerArcanne, PowerFirstInimigo)
+    if TempInimigo == '2':
+        if Protagonista['Classe'] == 'Servant':
+            Battle(PowerServant, PowerSecondInimigo)
+        if Protagonista['Classe'] == 'Logia':
+            Battle(PowerLogia, PowerSecondInimigo)
+        if Protagonista['Classe'] == 'Arcanne':
+            Battle(PowerArcanne, PowerSecondInimigo)
